4c1e00364986dbbe6d2fd6b8076809df53c36cb2
Gesiel Neves <neves.gesiel@gmail.com>
2022-05-27 14:23:15 +0044

    Criado classe Contatos

---
+++ src/main/java/com/devinhouse/agenda/AgendaGesielApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaGesielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaGesielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/agendagesiel/AgendaGesielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.agendagesiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaGesielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
